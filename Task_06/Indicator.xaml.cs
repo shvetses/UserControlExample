fdffa2971d3989074ed0c2661dc094b5993496ba
Екатерина Швец <shvetses@pik.ru>
2025-09-04 18:44:48 +0300

    fix

---
+++ Task_06/Indicator.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_06
+{
+    /// <summary>
+    /// Логика взаимодействия для Indicator.xaml
+    /// </summary>
+    public partial class Indicator : UserControl
+    {
+        public Indicator()
+        {
+            InitializeComponent();
+        }
+
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(double),
+                typeof(Indicator), 
+                new FrameworkPropertyMetadata(
+                    default(double),
+                    0,
+                    null,
+                    new CoerceValueCallback(CoerceMyValue))
+                );
+
+        private static object CoerceMyValue(DependencyObject d, object baseValue)
+        {
+            double v = (double) baseValue;
+            if ( v < 0 ) return 0;
+            if ( v > 180 ) return 180;
+            return v;
+        }
+
+        public double Value
+        {
+            get=>(double)GetValue(ValueProperty);
+            set=>SetValue(ValueProperty, value);
+        }
+    }
+}
